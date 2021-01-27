bafab87b6aab0cfcaf73548f8cde67718c656801
Justin Kirk <justinleekirk@gmail.com>
2021-01-27 17:25:01 -0600

    Fixing my programming environment

---
+++ First Missing Positive.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+    let max = 1, sortedNums = nums.sort(function(a,b){return a-b});
+    
+    for (let ele of sortedNums){
+        if (max < ele) return max;
+        else if (max == ele) max++;
+        else continue;
+    }
+    return max;
+};
+console.log(firstMissingPositive([1,2,0]));
+console.log(firstMissingPositive([3,4,-1,1]));
+console.log(firstMissingPositive([7,8,9,11,12]));
