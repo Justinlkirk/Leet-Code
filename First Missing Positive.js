68e4d948d39019bf71930d5a23c4526076a0bc80
Justin Kirk <justinleekirk@gmail.com>
2021-01-27 17:34:17 -0600

    Getting Git set up again

--- First Missing Positive.js
+++ First Missing Positive.js
@@ -15,3 +15,4 @@ var firstMissingPositive = function(nums) {
 console.log(firstMissingPositive([1,2,0]));
 console.log(firstMissingPositive([3,4,-1,1]));
 console.log(firstMissingPositive([7,8,9,11,12]));
+console.log(firstMissingPositive([1,2,3,4,5]));
